56c186f41e29be53883fe32984fe040c8ff839a3
vovchik-52 <narytnevv@gmail.com>
2025-02-22 17:55:40 +0700

    Delete full_vision.c

--- full_vision.c
+++
@@ -1,7 +0,0 @@
-#include "heder.h"
-int full_vision(struct abonent **ptr,int count){
-    for (int i = 0; i < count; i++)
-    {
-       printf("Абонент № %d\t Имя:%s\tФамилия:%s\tТелефон:%s\n",i, (*ptr)[i].name,(*ptr)[i].second_name,(*ptr)[i].tel);
-    }
-    }
